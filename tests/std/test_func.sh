779cd4ebb5efcfda26e4ba0da291d22317b2d7c7
Jakob Jordan <jakobjordan@posteo.de>
2024-02-01 15:38:16 -0500

    Add more tests

--- tests/std/test_func.sh
+++ tests/std/test_func.sh
@@ -7,3 +7,5 @@ assertsilent test_func.act test_pow
 assertsilent test_func.act test_ceil_log2
 assertsilent test_func.act test_max
 assertsilent test_func.act test_min
+assertsilent test_func.act test_repeat "cycle"
+assertsilent test_func.act test_sign_extend "cycle"
